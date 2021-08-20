eb3b03337d570751ecde9f73e0a173f7db958153
Vanshagra <vanshdeep.rai@gmail.com>
2021-08-20 19:44:45 +0530

    Changes done

--- Scroll/logic.js
+++ Scroll/logic.js
@@ -4,10 +4,22 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-let date = document.querySelector('.date');
+let date = document.querySelector(".date");
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+let navToggle = document.querySelector(".nav-toggle");
+let linksContainer = document.querySelector(".links-container");
+let links = document.querySelector(".links");
 
+navToggle.addEventListener("click", () => {
+  let linksContainerHeight = linksContainer.getBoundingClientRect().height;
+  let linksHeight = links.getBoundingClientRect().height;
+  if (linksContainerHeight == 0) {
+    linksContainer.style.height =`${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0 + "px";
+  }
+});
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
