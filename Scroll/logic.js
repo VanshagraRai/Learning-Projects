57d5a596ef6f428a7697197de9bdebc8f1dce774
Vanshagra <vanshdeep.rai@gmail.com>
2021-08-23 18:28:22 +0530

    Changes done

--- Scroll/logic.js
+++ Scroll/logic.js
@@ -14,13 +14,60 @@ let links = document.querySelector(".links");
 navToggle.addEventListener("click", () => {
   let linksContainerHeight = linksContainer.getBoundingClientRect().height;
   let linksHeight = links.getBoundingClientRect().height;
+
   if (linksContainerHeight == 0) {
-    linksContainer.style.height =`${linksHeight}px`;
+    linksContainer.style.height = `${linksHeight}px`;
   } else {
     linksContainer.style.height = 0 + "px";
   }
 });
 // ********** fixed navbar ************
+let navbar = document.getElementById("nav");
+let topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", () => {
+  let navbarHeight = navbar.getBoundingClientRect().height;
+  let scrollHeight = window.pageYOffset;
+  if (scrollHeight > navbarHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
 
 // ********** smooth scroll ************
 // select links
+let scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    // Preventing default
+    e.preventDefault();
+    //navigate to a specifit spot
+    let attribute = e.currentTarget.getAttribute("href").slice(1);
+    let section = document.getElementById(attribute);
+    // Calculate the heights
+    let navbarHeight = navbar.getBoundingClientRect().height;
+    let linksContainerHeight = linksContainer.getBoundingClientRect().height;
+    let fixedNavbar = navbar.classList.contains("fixed-nav");
+    let position = section.offsetTop;
+    position = position-navbarHeight;
+    if (!fixedNavbar) {
+      position -= navbarHeight; 
+    }
+    if (navbarHeight>82) {
+      position += linksContainerHeight;  
+    }
+    console.log(position);
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+});
