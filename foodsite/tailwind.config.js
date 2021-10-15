1c89aa89660a1749d4c076ae1ce9e95b3ee9f769
Vanshagra <vanshdeep.rai@gmail.com>
2021-10-15 13:32:54 +0530

    Changes done

---
+++ foodsite/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    color: {
+      primary:'#ff6363',
+      secondary: {
+        100 : '#E2E2d5', 
+        200 : '#888883',
+      }
+    },
+    extend: {
+     
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
